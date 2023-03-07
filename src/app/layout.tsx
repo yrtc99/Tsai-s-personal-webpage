aac6f47dabb96b9566cda7b07360ba4534292ef6
yrtc99 <yrtc99@gmail.com>
2023-03-08 00:55:09 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,19 +18,12 @@ import { fetchSkills } from 'utiles/fetchSkills'
 import { fetchProjects } from 'utiles/fetchProjects'
 
 import { NoSSR } from './components/NoSSR';
-import { GetStaticProps } from 'next'
 
 
-type Props = {
-  pageInfo: PageInfo;
-  experiences: Experience[];
-  skills: Skill[];
-  projects: Project[];
-  socials: Social[];
-}
 
 
-const layout = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
+
+const layout = () => {
   return (
 
     <html lang="en" className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#fe21d2]/80' >
@@ -45,7 +38,7 @@ const layout = ({ pageInfo, experiences, projects, skills, socials }: Props) => 
       <body>
         {/*header */}
         <NoSSR>
-          <Header socials={socials} />
+          <Header/>
         </NoSSR>
 
 
@@ -53,7 +46,7 @@ const layout = ({ pageInfo, experiences, projects, skills, socials }: Props) => 
         {/*intro*/}
         <NoSSR>
           <section id="intro" className='snap-start'>
-            <Intro pageInfo={pageInfo} />
+            <Intro />
           </section>
         </NoSSR>
 
@@ -61,7 +54,7 @@ const layout = ({ pageInfo, experiences, projects, skills, socials }: Props) => 
         {/*about */}
         <NoSSR>
           <section id="about" className='snap-center'>
-            <Aboutme pageInfo={pageInfo} />
+            <Aboutme />
           </section>
         </NoSSR>
 
@@ -69,21 +62,21 @@ const layout = ({ pageInfo, experiences, projects, skills, socials }: Props) => 
         {/*experience */}
         <NoSSR>
           <section id="experience" className='snap-center'>
-            <WorkExperience experiences={experiences} />
+            <WorkExperience  />
           </section>
         </NoSSR>
 
         <NoSSR>
           {/*skills */}
           <section id="skills" className='snap-start'>
-            <Skills skills={skills} />
+            <Skills />
           </section>
         </NoSSR>
 
         <NoSSR>
           {/*projects */}
           <section id="projects" className='snap-start'>
-            <Projects projects={projects} />
+            <Projects  />
           </section>
         </NoSSR>
 
@@ -101,24 +94,3 @@ const layout = ({ pageInfo, experiences, projects, skills, socials }: Props) => 
   )
 }
 export default layout;
-
-export async function getStaticProps() {
-  const pageInfo: PageInfo = await fetchPageInfo()
-  const experiences: Experience[] = await fetchExperiences()
-  const skills: Skill[] = await fetchSkills()
-  const projects: Project[] = await fetchProjects()
-  const socials: Social[] = await fetchSocial()
-
-  return {
-    props: {
-      pageInfo,
-      experiences,
-      skills,
-      projects,
-      socials,
-    },
-    //next.js will attempt to re-generate the page:
-    // when a reguest comes in a most once every 10 seconds
-    revalidate: 10,
-  }
-}
